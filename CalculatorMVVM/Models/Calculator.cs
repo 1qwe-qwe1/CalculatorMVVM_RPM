7a1003ba634ae35c530079f8a50bff9b60394cf8
1qwe-qwe1 <qudasetl@gmail.com>
2026-04-30 20:48:31 +0700

    Добавьте файлы проекта.

---
+++ CalculatorMVVM/Models/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CalculatorMVVM.Models
+{
+    public class Calculator : ICalculator
+    {
+        public double Result { get; private set; }
+
+        public void Evaluate(string expression)
+        {
+            if (string.IsNullOrWhiteSpace(expression))
+            {
+                throw new ArgumentException("Выражение не может быть пустым");
+            }
+
+            if (!Regex.IsMatch(expression, @"^[0-9+\-*/()\s.,]+$"))
+            {
+                throw new ArgumentException("Выражение содержит недопустимые символы");
+            }
+
+            try
+            {
+                var dataTable = new DataTable();
+                var result = dataTable.Compute(expression, "");
+                Result = Convert.ToDouble(result);
+            }
+            catch (EvaluateException)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException($"Ошибка при вычислении выражения: {ex.Message}");
+            }
+        }
+    }
+}
